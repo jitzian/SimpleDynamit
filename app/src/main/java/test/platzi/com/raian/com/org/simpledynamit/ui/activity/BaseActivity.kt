a2ec71fd5a90b414adeae3c4ce99f03db8bb0341
raian <you@example.com>
2018-04-30 22:53:46 -0400

    Adding Simple BaseActivity

--- app/src/main/java/test/platzi/com/raian/com/org/simpledynamit/ui/activity/BaseActivity.kt
+++ app/src/main/java/test/platzi/com/raian/com/org/simpledynamit/ui/activity/BaseActivity.kt
@@ -1,2 +1,7 @@
 package test.platzi.com.raian.com.org.simpledynamit.ui.activity
 
+import android.support.v7.app.AppCompatActivity
+
+abstract class BaseActivity: AppCompatActivity() {
+    abstract fun initializeView()
+}
