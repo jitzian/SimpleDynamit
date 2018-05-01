783594cfa095b910af417b0fc845ac9bebb6a911
raian <you@example.com>
2018-04-30 20:47:04 -0400

    Adding remote URL for remote image

--- app/src/main/java/test/platzi/com/raian/com/org/simpledynamit/constants/GlobalConstants.kt
+++ app/src/main/java/test/platzi/com/raian/com/org/simpledynamit/constants/GlobalConstants.kt
@@ -7,5 +7,7 @@ class GlobalConstants{
         const val ERROR_CREDENTIALS_INVALID = "Credentials are invalid"
         const val BASE_URL_OPEN_AQ = "https://api.openaq.org"
         const val OPEN_AQ_CITIES_LIMIT = 2046
+        const val MEASUREMENTS_FILTER = 10000
+        const val URL_IMAGE_URL = "http://www.luxus-india.com/wp-content/uploads/2015/12/stock-vector-flat-city-icon-209365492.png"
     }
 }
